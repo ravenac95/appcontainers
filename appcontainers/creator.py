a5a628f48977b444f8312d1097bd1c2cfdb1f249
Reuven V. Gonzales <reuven@tobetter.us>
2012-07-13 15:49:35 -0700

    Developing the AppContainerCreator
    Doing a bit of a spike at the moment

---
+++ appcontainers/creator.py
@@ -0,0 +1,38 @@
+class AppContainerCreator(object):
+    """Coordinates the creation of a new AppContainer"""
+    def __init__(self, settings, app_container_cls, 
+            lxc_service, file_assembler):
+        self._settings = settings
+        self._app_container_cls = app_container_cls
+        self._lxc_service = lxc_service
+        self._file_assembler = file_assembler
+
+    def provision_container(self, base, reservation):
+        """Provisions a brand new container
+        
+        :param reservation: A ResourceReservation object that describes
+            a container's resources
+        """
+        settings = self._settings
+        app_container_cls = self._app_container_cls
+
+        # Create the overlay director(y|ies)
+        overlays = self._create_overlay_directories(reservation.name)
+
+        # Create the LXC object
+        lxc = self._create_lxc(reservation.name, base, overlays)
+
+        # Setup the files in the LXC
+        self._file_assembler.setup(lxc, reservation)
+
+        # Create and return an app container for the LXC and it's reservations
+        return app_container_cls.create(base, lxc, reservation)
+
+    def _create_overlay_directories(self, name):
+        """Creates overlay directories"""
+        top_overlay = self._settings.overlays_path(name)
+        return [top_overlay]
+
+    def _create_lxc(self, name, base, overlays):
+        """Creates the LXC object from the given name and overlays"""
+        return self._lxc_service.create(name, base=base, overlays=overlays)
---
+++ tests/test_creator.py
@@ -0,0 +1,35 @@
+from mock import Mock, patch
+from appcontainers.creator import *
+from tests.fakes import *
+
+class TestAppContainerCreator(object):
+    def setup(self):
+        mock_settings = Mock()
+        mock_app_container_cls = Mock()
+        mock_lxc_service = Mock()
+        mock_file_assembler = Mock()
+
+        creator = AppContainerCreator(mock_settings, 
+                app_container_cls=mock_app_container_cls,
+                lxc_service=mock_lxc_service,
+                file_assembler=mock_file_assembler)
+        self.creator = creator
+
+        self.mock_settings = mock_settings
+        self.mock_app_container_cls = mock_app_container_cls
+        self.mock_lxc_service = mock_lxc_service
+        self.mock_file_loader = mock_file_loader
+    
+    def test_app_container_creator_provision_container(self):
+        fake_reservation = FakeResourceReservation.create('somename', 
+                '192.168.0.1', '00:16:3e:00:00:00')
+        container = self.creator.provision_container('base', fake_reservation)
+        # Mock Assertions
+        self.mock_file_loader.setup.assert_called_with(
+                self.mock_lxc_service.create.return_value)
+
+
+        # Assert return value
+        assert container == self.mock_app_container_cls.create.return_value
+
+        
