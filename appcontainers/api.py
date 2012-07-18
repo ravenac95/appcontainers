668b7e2b7c75b4cc6809bc2af0354fb3a272ad9e
Reuven V. Gonzales <reuven@tobetter.us>
2012-07-18 12:29:59 -0700

    Some aesthetics

--- appcontainers/api.py
+++ appcontainers/api.py
@@ -4,27 +4,13 @@ appcontainer.api
 
 The appcontainers API
 """
-from . import constants
+import lxc4u
 from .service import AppContainerService
 from .resources import setup_resource_service
 from .creator import setup_app_container_creator
 from .settings import Settings
         
-import os
 
-class LXC(object):
-
-    local_test = os.path.abspath('localer')
-    def path(self, *join_paths):
-        return os.path.join(self.local_test, *join_paths)
-
-
-
-class lxc4u(object):
-    @classmethod
-    def create(cls, *args, **kwargs):
-        print "NOT ACTUALLY CREATING AN LXC"
-        return LXC()
 
 def setup_service():
     """Creates an AppContainerService based on the settings"""
@@ -34,12 +20,14 @@ def setup_service():
     app_container_creator = setup_app_container_creator(settings, lxc4u)
     return AppContainerService(resource_service, app_container_creator)
 
+
 def _create_settings(**kwargs):
     import ipaddr
     settings = Settings(base_path='/var/lib/appcontainers', 
             network=ipaddr.IPv4Network('192.168.0.0/24'),
             mac_range=['00:16:3e:00:00:00', '00:16:3e:00:01:00'])
     return settings
+
 
 def _create_resource_repository():
     """Creates a resource repository"""
@@ -51,6 +39,7 @@ def _create_resource_repository():
         ('charlie', '192.168.0.3', '00:16:3e:00:00:02'),
     ])
     return resource_repository
+
 
 def _wire_dependencies():
     """Wires the dependencies for the AppContainerService"""
